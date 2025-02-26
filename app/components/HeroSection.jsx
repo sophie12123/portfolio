4328dd1655835164592c83888ede15002e2c149e
sophie12123 <127309133+sophie12123@users.noreply.github.com>
2025-02-26 22:22:11 +1100

    Update HeroSection.jsx

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -48,7 +48,7 @@ const HeroSection = () => {
         <div className="col-span-4 place-self-center  mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png "
+              src="https://portfolio-sophie12123s-projects.vercel.app/images/hero-image.png "
               alt="my pic"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
